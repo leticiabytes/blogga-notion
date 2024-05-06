4cc0a71cc2f81fe150749568b60fbfdc73a6943d
Letícia Vieira <lvieira268@gmail.com>
2024-05-06 20:02:00 -0300

    feat: add footer and posts components
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
+import { Posts } from "@/components/Posts";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col mx-auto p-10">
       <Header />
+      <Posts />
+      <Footer />
     </main>
   );
 }
