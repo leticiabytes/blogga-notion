fa5e3323351f07bd20fe02dd08bc324a50fe39e0
Letícia Vieira <lvieira268@gmail.com>
2024-05-07 16:25:06 -0300

    feat: configure custom font
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { graphik } from "@/lib/font";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Blogga Notion",
@@ -16,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={graphik.className}>{children}</body>
     </html>
   );
 }
