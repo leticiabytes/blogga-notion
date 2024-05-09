a52fca9e6d57b5865897766a8876b4ce8ee3f25a
Letícia Vieira <lvieira268@gmail.com>
2024-05-08 21:07:21 -0300

    feat: creates posts model
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

---
+++ src/models/posts-model.ts
@@ -0,0 +1,175 @@
+export interface PostsModel {
+  object: string;
+  id: string;
+  created_time: string;
+  last_edited_time: string;
+  created_by: CreatedBy;
+  last_edited_by: LastEditedBy;
+  cover: any;
+  icon: any;
+  parent: Parent;
+  archived: boolean;
+  properties: Properties;
+  url: string;
+  public_url: any;
+}
+
+export interface CreatedBy {
+  object: string;
+  id: string;
+}
+
+export interface LastEditedBy {
+  object: string;
+  id: string;
+}
+
+export interface Parent {
+  type: string;
+  database_id: string;
+}
+
+export interface Properties {
+  Category: Category;
+  Description: Description;
+  Slug: Slug;
+  Status: Status;
+  BannerImage: BannerImage;
+  Title: Title;
+}
+
+export interface Category {
+  id: string;
+  type: string;
+  select: Select;
+}
+
+export interface Select {
+  id: string;
+  name: string;
+  color: string;
+}
+
+export interface Description {
+  id: string;
+  type: string;
+  rich_text: RichText[];
+}
+
+export interface RichText {
+  type: string;
+  text: Text;
+  annotations: Annotations;
+  plain_text: string;
+  href: any;
+}
+
+export interface Text {
+  content: string;
+  link: any;
+}
+
+export interface Annotations {
+  bold: boolean;
+  italic: boolean;
+  strikethrough: boolean;
+  underline: boolean;
+  code: boolean;
+  color: string;
+}
+
+export interface Slug {
+  id: string;
+  type: string;
+  rich_text: RichText2[];
+}
+
+export interface RichText2 {
+  type: string;
+  text: Text2;
+  annotations: Annotations2;
+  plain_text: string;
+  href: any;
+}
+
+export interface Text2 {
+  content: string;
+  link: any;
+}
+
+export interface Annotations2 {
+  bold: boolean;
+  italic: boolean;
+  strikethrough: boolean;
+  underline: boolean;
+  code: boolean;
+  color: string;
+}
+
+export interface Status {
+  id: string;
+  type: string;
+  status: Status2;
+}
+
+export interface Status2 {
+  id: string;
+  name: string;
+  color: string;
+}
+
+export interface BannerImage {
+  id: string;
+  type: string;
+  url: string;
+}
+
+export interface RichText3 {
+  type: string;
+  text: Text3;
+  annotations: Annotations3;
+  plain_text: string;
+  href: any;
+}
+
+export interface Text3 {
+  content: string;
+  link: any;
+}
+
+export interface Annotations3 {
+  bold: boolean;
+  italic: boolean;
+  strikethrough: boolean;
+  underline: boolean;
+  code: boolean;
+  color: string;
+}
+
+export interface Title {
+  id: string;
+  type: string;
+  title: Title2[];
+}
+
+export interface Title2 {
+  type: string;
+  text: Text4;
+  annotations: Annotations4;
+  plain_text: string;
+  href: any;
+}
+
+export interface Text4 {
+  content: string;
+  link: any;
+}
+
+export interface Annotations4 {
+  bold: boolean;
+  italic: boolean;
+  strikethrough: boolean;
+  underline: boolean;
+  code: boolean;
+  color: string;
+}
