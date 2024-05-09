2253dc55ba32f7328c83824327686655ecb5eafa
Letícia Vieira <lvieira268@gmail.com>
2024-05-08 21:07:40 -0300

    feat: creates get-notion-database service
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

---
+++ src/services/get-notion-database.ts
@@ -0,0 +1,18 @@
+import "server-only";
+
+import { notionClient } from "./notion-client";
+import { PostsModel } from "@/models/posts-model";
+
+export async function notionDatabase() {
+  const posts = await notionClient.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID!,
+    filter: {
+      property: "Status",
+      status: {
+        equals: "Done",
+      },
+    },
+  });
+
+  return posts.results as PostsModel[];
+}
