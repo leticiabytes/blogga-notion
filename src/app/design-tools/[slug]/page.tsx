469b2548b85005cfb2215c6fb9c0559b6620cd22
Letícia Vieira <lvieira268@gmail.com>
2024-05-14 21:55:10 -0300

    feat: init slug design-tools page
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

---
+++ src/app/design-tools/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import PagePost from "@/components/page-post";
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  return (
+    <div>
+      <p>ola</p>
+    </div>
+  );
+}
