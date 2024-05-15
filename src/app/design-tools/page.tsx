d77d81041661046fc9f981954f5a12a8fbc4e0d5
Letícia Vieira <lvieira268@gmail.com>
2024-05-14 21:55:49 -0300

    feat: updates design-tools page
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

--- src/app/design-tools/page.tsx
+++ src/app/design-tools/page.tsx
@@ -1,11 +1,5 @@
 import { Posts } from "@/components/posts";
 
 export default async function DesingTools() {
-  return (
-    <main className="min-h-screen">
-      <div className="flex min-h-screen flex-col mx-auto p-10">
-        <Posts />
-      </div>
-    </main>
-  );
+  return <Posts />;
 }
